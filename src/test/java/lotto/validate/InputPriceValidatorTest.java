b9df1602a42af8bd10f2232f1bf63043d65585d7
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-11-03 15:50:15 +0900

    feat(validation) : 불필요한 import문 정리

--- src/test/java/lotto/validate/InputPriceValidatorTest.java
+++ src/test/java/lotto/validate/InputPriceValidatorTest.java
@@ -1,9 +1,6 @@
 package lotto.validate;
 
-import lotto.Lotto;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
