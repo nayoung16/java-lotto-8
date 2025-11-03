fe4cc6ba402d9c7737315c3f8088fdb1ec713ac8
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-11-03 15:50:45 +0900

    feat(view) : 입력 형식 맞추기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,13 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 import lotto.validate.InputPriceValidator;
 import lotto.validate.LottoNumValidator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
@@ -10,41 +15,46 @@ public class InputView {
     private static final String INPUT_NUM = "당첨번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUM = "보너스 번호를 입력해 주세요.";
 
-    public static void inputPriceAndCount() {
+    public static int inputPriceAndCount() {
         while (true) {
             try {
                 System.out.println(INPUT_PRICE);
                 int cost = parseIntOrThrow(Console.readLine());
                 InputPriceValidator.validatePrice(cost);
+                System.out.println();
                 int lottoCount = cost / 1000;
                 System.out.println(lottoCount + PRICE_MSG);
-                break;
+                return lottoCount;
             } catch (IllegalArgumentException | IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public static void inputWinningNum() {
+    public static Lotto inputWinningNum() {
         while (true) {
             try {
                 System.out.println(INPUT_NUM);
                 String winningNum = Console.readLine();
                 LottoNumValidator.isValidString(winningNum);
-                break;
+                List<Integer> parsedNumList = parseList(winningNum);
+                System.out.println();
+                return new Lotto(parsedNumList);
             } catch (IllegalArgumentException | IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public static void inputBonusNum() {
+    public static int inputBonusNum() {
         while (true) {
             try {
                 System.out.println(INPUT_BONUS_NUM);
                 String bonusNum = Console.readLine();
-                LottoNumValidator.isValidNumber(parseIntOrThrow(bonusNum));
-                break;
+                int parsedNum = parseIntOrThrow(bonusNum);
+                LottoNumValidator.isValidNumber(parsedNum);
+                System.out.println();
+                return parsedNum;
             } catch (IllegalArgumentException | IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
@@ -57,5 +67,12 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
         }
+    }
+
+    private static List<Integer> parseList(String s) {
+        return Arrays.stream(s.split(","))
+                .map(String::trim)
+                .map(InputView::parseIntOrThrow)
+                .collect(Collectors.toList());
     }
 }
